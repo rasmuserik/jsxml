3d59ad97e81c24c3a69a9e02f85125f056b72c68
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-02-05 21:14:35 +0100

    testing

--- test/test.js
+++ test/test.js
@@ -1,1 +1,7 @@
-console.log("TODO");
+var jsxml = require('../lib/jsxml.js');
+var fs = require('fs');
+var musicstr = fs.readFileSync('test/music.xml').toString();
+var musicjson = jsxml.fromXml(musicstr);
+
+console.log(jsxml.toXml(musicjson), musicstr);
+
